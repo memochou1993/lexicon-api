9a99044b41a3b0126828e43f4891332b194a27cf
Memo Chou <memochou1993@gmail.com>
2020-05-10 00:01:52 +0800

    Fix wrong resource in KeyResource

--- app/Http/Resources/KeyResource.php
+++ app/Http/Resources/KeyResource.php
@@ -18,7 +18,7 @@ class KeyResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'project' => new ProjectResource($this->whenLoaded('project')),
-            'values' => ProjectResource::collection($this->whenLoaded('values')),
+            'values' => ValueResource::collection($this->whenLoaded('values')),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
