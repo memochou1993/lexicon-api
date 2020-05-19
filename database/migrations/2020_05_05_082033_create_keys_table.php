a17fd07d7b4924c34e406054a8be2051d428808a
Memo Chou <memochou1993@gmail.com>
2020-05-19 23:42:06 +0800

    Modify migrations

--- database/migrations/2020_05_05_082033_create_keys_table.php
+++ database/migrations/2020_05_05_082033_create_keys_table.php
@@ -15,7 +15,7 @@ class CreateKeysTable extends Migration
     {
         Schema::create('keys', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->index();
             $table->foreignId('project_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
