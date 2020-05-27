a2a394385728b0d789ec73e0292c42d218e50920
Memo Chou <memochou1993@gmail.com>
2020-05-27 20:33:28 +0800

    Modify api route

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::namespace('Api')->group(function () {
     Route::prefix('auth')->group(function () {
         Route::post('register', 'AuthController@register');
@@ -62,10 +51,10 @@ Route::namespace('Api')->group(function () {
                 'destroy',
             ],
         ]);
+    });
 
-        // TODO
-        // Route::namespace('Client')->prefix('client')->group(function () {
-        //     Route::get('keys', 'KeyController@index');
-        // });
+    Route::namespace('Client')->prefix('client')->group(function () {
+        Route::apiResource('projects.keys', 'KeyController')
+            ->only('index');
     });
 });
