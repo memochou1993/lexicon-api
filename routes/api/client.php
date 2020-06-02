2fe81192bde6c1199ec5828931a684d24a2781dd
Memo Chou <memochou1993@gmail.com>
2020-06-02 16:52:00 +0800

    Modify route for client

--- routes/api/client.php
+++ routes/api/client.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('projects', 'ProjectController')
-    ->only('show');
+Route::get('projects/{project}', 'ProjectController@show');
 Route::delete('projects/{project}/cache', 'ProjectCacheController@destroy');
