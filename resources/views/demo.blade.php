01983991ed3208a56b6dcfe15ab911f330e3fb1f
Memo Chou <memochou1993@gmail.com>
2020-10-23 01:03:30 +0800

    Add loading image

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>New Project</title>
         <link rel="icon" href="icon.png">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     </head>
     <body>
         <nav class="navbar navbar-dark bg-dark">
@@ -28,12 +28,12 @@
                 <div class="card bg-light">
                     <div class="card-body">
                         <span class="mr-2">
-                            <button onclick="javascript:location.href='?language={{ $language  }}&sync=true'" class="btn btn-sm btn-info my-1 my-md-0">
+                            <button onclick="javascript:location.href='?language={{ $language  }}&sync=true'" class="btn btn-sm btn-info my-1 my-md-0" id="sync">
                                 Sync Language Files
                             </button>
                         </span>
                         <span class="mr-2">
-                            <button onclick="javascript:location.href='?language={{ $language  }}&clear=true'" class="btn btn-sm btn-danger my-1 my-md-0">
+                            <button onclick="javascript:location.href='?language={{ $language  }}&clear=true'" class="btn btn-sm btn-danger my-1 my-md-0" id="clear">
                                 Clear Language Files
                             </button>
                         </span>
@@ -47,7 +47,7 @@
                     </div>
                 </div>
             </div>
-            <div class="my-4">
+            <div class="my-4" id="table">
                 @if(count($keys))
                 <table class="table table-bordered table-responsive-sm bg-light">
                     <thead>
@@ -98,9 +98,32 @@
                 </table>
                 @endif
             </div>
+            <div class="my-5 text-center" id="loading" hidden>
+                <h5 class="py-3" id="message"></h5>
+                <div style="width: 4rem; height: 4rem;" class="spinner-grow text-warning" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
         </div>
     </body>
 </html>
+
+<script>
+document.getElementById('sync').addEventListener('click', () => {
+    document.getElementById('table').hidden = true;
+    document.getElementById('loading').hidden = false;
+    document.getElementById('message').innerHTML = 'Fetching resources...';
+    setTimeout(() => {
+        document.getElementById('message').innerHTML = 'Generating language files...';
+    }, 1000);
+});
+
+document.getElementById('clear').addEventListener('click', () => {
+    document.getElementById('table').hidden = true;
+    document.getElementById('loading').hidden = false;
+    document.getElementById('message').innerHTML = 'Deleting language files...';
+});
+</script>
 
 <style>
 body {
