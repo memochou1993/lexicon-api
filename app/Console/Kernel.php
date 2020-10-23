4b11ee2b570a04845df0f0e8b52ae058638d6169
Memo Chou <memochou1993@gmail.com>
2020-10-23 22:34:31 +0800

    Modify Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        SyncCommand::class,
         ClearCommand::class,
+        SyncCommand::class,
     ];
 
     /**
